583cf313466134fae6fd0f24c5ebd1f19c9c6251
Cai-Lin-1166249 <cai.lin@lincolnuni.ac.nz>
2026-04-14 14:34:26 +1200

    modify the hardcoded base url

--- frontend_service/src/api.js
+++ frontend_service/src/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://127.0.0.1/api"
+  // baseURL: "http://127.0.0.1/api"
+  baseURL: "https://realtime-crypto.xyz/"
 });
 
 export default API;
